65ff811d8c9de8b460364df0c6898b87ae4429d5
Richard Lawson <lawson.a.richard@gmail.com>
2019-09-16 14:39:14 -0700

    added ability to post username/password
    username and password POST with bcryptjs

--- users/users-router.js
+++ users/users-router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const bcrypt = require("bcryptjs");
 const users = require("./users.model");
 
 const router = express.Router();
@@ -12,6 +12,21 @@ router.get("/", (req, res) => {
     })
     .catch(err => {
       res.status(500).json({ error: "Error in GETTING users" });
+    });
+});
+
+router.post("/", (req, res) => {
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password);
+  user.password = hash;
+  users
+    .add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Cannot add user" });
     });
 });
 
