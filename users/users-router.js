88ed68a68d201a311270dfb48df577143e1f4e03
Richard Lawson <lawson.a.richard@gmail.com>
2019-09-16 14:45:39 -0700

    added post
    added login for users

--- users/users-router.js
+++ users/users-router.js
@@ -30,4 +30,20 @@ router.post("/", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  users
+    .findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ error: "Invalid Credentials, try again" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 module.exports = router;
